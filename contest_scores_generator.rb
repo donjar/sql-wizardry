76a04400cbead70855cad86f1189b975faced7c8
Herbert Ilhan Tanujaya <mail@herbert.id>
2017-09-22 13:15:10 +0800

    lint Ruby files

--- contest_scores_generator.rb
+++ contest_scores_generator.rb
@@ -1,6 +1,7 @@
 1.upto(500) do |uid|
   1.upto(3) do |pid|
-    next if (uid + pid) % 7 == 0
-    puts "INSERT INTO contest_scores VALUES (#{uid}, #{pid}, #{Random.rand(10)});"
+    next if ((uid + pid) % 7).zero?
+    score = Random.rand(10)
+    puts "INSERT INTO contest_scores VALUES (#{uid}, #{pid}, #{score});"
   end
 end
--- nus_students_generator.rb
+++ nus_students_generator.rb
@@ -1,8 +1,11 @@
 1000.times do
-  matric = "A" + Random.rand(1000000).to_s.rjust(7, "0") + ("A".."Z").to_a.sample
-  date = Random.rand(2010..2015).to_s + '-' + Random.rand(1..12).to_s.rjust(2, '0') + '-' + Random.rand(1..28).to_s.rjust(2, '0')
-  interval = Random.rand(3..4).to_s + ' years ' + Random.rand(1..12).to_s + ' months'
-  faculty = %w(Computing Engineering Science Arts).sample
+  matric = "A#{Random.rand(1_000_000).to_s.rjust(7, '0')}" \
+    "#{('A'..'Z').to_a.sample}"
+  date = "#{Random.rand(2010..2015)}-#{Random.rand(1..12).to_s.rjust(2, '0')}" \
+    "-#{Random.rand(1..28).to_s.rjust(2, '0')}"
+  interval = "#{Random.rand(3..4)} years #{Random.rand(1..12)} months"
+  faculty = %w[Computing Engineering Science Arts].sample
   cap = Random.rand(3.0..5.0).round(2)
-  puts "INSERT INTO nus_students VALUES ('#{matric}', '#{date}', '#{interval}', '#{faculty}', '#{cap}');"
+  puts "INSERT INTO nus_students VALUES ('#{matric}', '#{date}', " \
+    "'#{interval}', '#{faculty}', '#{cap}');"
 end
